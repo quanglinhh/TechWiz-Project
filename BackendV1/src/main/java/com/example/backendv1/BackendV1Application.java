6f55e2d03c8a75df6990333a7a6950abaad3411e
henrydao15 <dinhluyen45@gmail.com>
2022-08-10 14:44:57 +0700

    Update BackendV1Application.java

--- BackendV1/src/main/java/com/example/backendv1/BackendV1Application.java
+++ BackendV1/src/main/java/com/example/backendv1/BackendV1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendV1Application {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendV1Application.class, args);
     }
