7c838dc42ab92401f6dd3aef4959753958864b97
Bui Van Trung <84084472+trungmark@users.noreply.github.com>
2022-08-11 14:38:56 +0700

    trung update

--- BackendV1/src/main/java/com/example/backendv1/DrugDosages/Model/DrugDosages.java
+++ BackendV1/src/main/java/com/example/backendv1/DrugDosages/Model/DrugDosages.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
-@Table(name = "drug_dosages", schema = "dbo", catalog = "HealthManage")
+@Table(name = "drug_dosages", schema = "dbo")
 @Getter
 @Setter
 @AllArgsConstructor
